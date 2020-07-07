ae64fac6bdf9303a8048007040c744994b2a53b8
ECDESENVOLVIMENTO02 <molx321@homtail.com>
2020-07-06 23:23:35 -0300

    Login Page - Part 4

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController } from '@ionic/angular';
+import { AlertController, NavController, LoadingController } from '@ionic/angular';
 import { UrlService } from 'src/app/provider/url.service';
 import { map } from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { pipe } from 'rxjs';
 
 
@@ -17,7 +17,7 @@ export class LoginPage implements OnInit {
   senha: string;
 
 
-  constructor(public alert: AlertController, public urlService: UrlService, public http: HttpClient, public naav: NavController) { }
+  constructor(public alert: AlertController, public urlService: UrlService, public http: HttpClient, public naav: NavController, public loading:LoadingController) { }
 
   ngOnInit() {
   }
@@ -25,25 +25,39 @@ export class LoginPage implements OnInit {
   async logar() {
     //Verifica se algum dos campos não está preenchido
     if (this.email == undefined || this.senha == undefined) {
+      this.urlService.alertas('Atenção', 'Preencha todos os campos');
 
-      const alert = await this.alert.create({
-        header: 'Atenção',
-        message: 'Preencha todos os campos!',
-        buttons: ['OK']
-      });
-      await alert.present();
 
     } else {//Se os campos forem preenchidos
-      
-      this.http.get(this.urlService.getURL()+"login.php?email="+this.email+"&senha="+this.senha)
-      .subscribe(
-          
-          (data:any) => {
-            //Se o get retornou a mensagem de "sim" para o subcampo "logado" do campo "msg", o login foi feito com sucesso
-           if(data.msg.logado=="sim"){
-              //Vai para home
-              this.naav.navigateBack('home');
+      //Tela de carergamento
+      const load = await this.loading.create({
+        message: 'Verificando login...',
+        spinner: "bubbles"
+      });
+      await load.present();
 
+      this.http.get(this.urlService.getURL() + "login.php?email=" + this.email + "&senha=" + this.senha)
+        .subscribe(
+
+          (data: any) => {
+            //Se o get retornou a mensagem de "sim" para o subcampo "logado" do campo "msg", o login foi feito com sucesso
+            if (data.msg.logado == "sim") {
+              //Verifica se o usuário está ativo no banco de dados
+              if(data.dados.status == "Ativo"){
+                //Encerra tela de carregamento
+                load.dismiss();
+                //Vai para home
+                this.naav.navigateBack('home');
+              }else{
+                //Encerra tela de carregamento
+                load.dismiss();
+                this.urlService.alertas('Atenção', 'Usuário Bloqueado!');
+              }              
+
+            }else{
+              //Encerra tela de carregamento
+              load.dismiss();
+              this.urlService.alertas('Atenção', 'Usuário ou senha incorretos!')
             }
           }
         );
--- src/app/provider/url.service.ts
+++ src/app/provider/url.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 //URL para chamar os arquivos PHP que fazem a manipulação de dados
 
@@ -8,10 +9,24 @@ import { Injectable } from '@angular/core';
 })
 export class UrlService {
 
-    url: string = "http://localhost/cursos_udemy/Catalogo/php/";
-  constructor() { }
+  url: string = "http://localhost/cursos_udemy/Catalogo/php/";
+
+  constructor(public alert: AlertController) { }
 
   getURL(){
       return this.url;
+  }
+
+
+  //Mensagem genérica de alerta com botão de 'OK'
+  async alertas(titulo, msg){
+    const alert = await this.alert.create({
+      header: titulo,
+      message: msg,
+      buttons: [
+        'OK'
+      ]
+    });
+    await alert.present();
   }
 }
